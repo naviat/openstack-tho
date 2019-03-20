2a48d8cbfea151d9601b47fafac083e040610cdc
naviat <haidv.ict@gmail.com>
2019-03-21 00:17:45 +0700

    uninstall ctl

---
+++ CentOS7.8.1804-Rocky-OVS/com1-00-uninstall-all.sh
@@ -0,0 +1,67 @@
+#!/bin/bash
+#Author Dam Van Hai
+
+source function.sh
+source config.sh
+
+
+# Function uninstall components
+components_uninstall () {
+	echocolor "Uninstall components"
+	sleep 3
+	yum remove -y '*openstack*' '*nova*' '*neutron*'
+
+	echocolor "Restore Network config"
+	cat << EOF > /etc/sysconfig/network-scripts/ifcfg-$COM1_EXT_IF
+DEVICE=$COM1_EXT_IF
+BOOTPROTO=none
+ONBOOT=yes
+IPADDR=$COM1_EXT_IP
+NETMASK=$COM1_EXT_NETMASK
+GATEWAY=$GATEWAY_EXT_IP
+DNS1=8.8.8.8
+DNS2=8.8.4.4
+
+EOF
+
+}
+
+# Function uninstall openvswitch
+openvswitch_uninstall() {
+	echocolor "Uninstall openvswitch"
+	sleep 3
+	if ! [ -x "$(command -v ovs-vsctl)" ]; then 
+		echocolor "You are not install it before, right!"
+	else
+		ovs-vsctl del-br br-provider
+		ovs-vsctl del-br br-tun
+		ovs-vsctl del-br br-int
+		yum remove -y openvswitch
+	fi
+	sleep 3
+	echocolor "--Done--"
+
+}
+
+# Function clean the components lib/log
+clean_lib_log () {
+	echocolor "Clean the components lib/log"
+	sleep 3
+
+	rm -rf /var/lib/nova /etc/nova /etc/neutron /etc/openvswitch /var/log/nova /var/log/openvswitch /var/log/neutron /sbin/ifup-local /etc/sysconfig/network-scripts/ifcfg-br-provider
+	sleep 3
+
+    echocolor "---DONE---"    
+}
+
+
+## Uninstall components
+components_uninstall
+
+## Uninstall openvswitch
+openvswitch_uninstall
+
+## Cleaning
+clean_lib_log
+
+reboot
--- CentOS7.8.1804-Rocky-OVS/ctl-00-uninstall.sh
+++ CentOS7.8.1804-Rocky-OVS/ctl-00-uninstall.sh
@@ -45,7 +45,38 @@ uninstall_ops_packages () {
 	# Warning! Dangerous step! Deletes local application data
 	rm -rf /root/.my.cnf /var/lib/glance /var/lib/nova /etc/nova /etc/swift \
 	/srv/node/device*/* /var/log/keystone
+
+	echocolor "Restore Network config"
+	cat << EOF > /etc/sysconfig/network-scripts/ifcfg-$CTL_EXT_IF
+DEVICE=$CTL_EXT_IF
+BOOTPROTO=none
+ONBOOT=yes
+IPADDR=$CTL_EXT_IP
+NETMASK=$CTL_EXT_NETMASK
+GATEWAY=$GATEWAY_EXT_IP
+DNS1=8.8.8.8
+DNS2=8.8.4.4
+
+EOF
 }
+
+# Function uninstall openvswitch
+uninstall_openvswitch() {
+	echocolor "Uninstall openvswitch"
+	sleep 3
+	if ! [ -x "$(command -v ovs-vsctl)" ]; then 
+		echocolor "You are not install it before, right!"
+	else
+		ovs-vsctl del-br br-provider
+		ovs-vsctl del-br br-tun
+		ovs-vsctl del-br br-int
+		yum remove -y openvswitch
+	fi
+	sleep 3
+	echocolor "--Done--"
+
+}
+
 
 uninstall_sql
 
@@ -54,5 +85,7 @@ uninstall_mq
 uninstall_memcached
 
 uninstall_ops_packages
+
+uninstall_openvswitch
 
 echo -e "\033[1;31m -REMOVED- \033[0m"
